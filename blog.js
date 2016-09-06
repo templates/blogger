979ac1b7633578eac2747dfabcaceb6d13e53731
templates <adelkandil120@gmail.com>
2016-09-06 07:04:19 +0200

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,12 @@
+﻿<script type='text/javascript'>
+//<![CDATA[
+window.onload = function(){ 
+    var copy=document.getElementById("credit");     
+    if(copy==null){
+        window.location.href="http://itqanwebs.blogspot.com/";
+    };
+    copy.setAttribute("href","http://itqanwebs.blogspot.com/");
+    copy.innerHTML="تعريب : مدونة إتقان ويب"
+};
+//]]>
+</script>
